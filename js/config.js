f5a24c0b90c1f494593cbb364ca315e9dbeb624f
Wright <treasurendubuisi21@gmail.com>
2026-06-04 20:27:21 +0100

    Reworked .gitignore

---
+++ js/config.js
@@ -0,0 +1,6 @@
+// js/config.js
+const CONFIG = {
+  SUPABASE_URL: 'https://xbkxjlhbeprxaebzcfwy.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhia3hqbGhiZXByeGFlYnpjZnd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1MzE1MTQsImV4cCI6MjA5NjEwNzUxNH0._uW1IoWXCB42pR2o2nJftj6ICicQwOGFPWfOESLKrXI',
+  ESPEES_RATE: 2050  // 1 Espees = 2050 NGN (fixed rate)
+};
